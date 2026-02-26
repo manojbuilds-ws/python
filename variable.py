6fbd1234bfcaa010c2c0e95a8193766df5c9ee4b
Manoj Kumar <143874196+manojbuilds-ws@users.noreply.github.com>
2026-02-26 14:18:57 +0530

    Reformat variable.py with added comments

--- variable.py
+++ variable.py
@@ -10,7 +10,9 @@ persentege=90.5
 #boolean
 is_student= True
 
+#print function
 print(name, roll_number, persentege, is_student)
 
 persentege=85.90
 print(name, roll_number, persentege, is_student)
+
